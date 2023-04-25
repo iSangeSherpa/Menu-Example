be6b35b3bdd227d4a2655b825a16fb4da9b429f7
Sange Sherpa <sherpasange1215@gmail.com>
2023-04-25 08:28:20 +0545

    Some Changes

--- app/src/main/java/np/edu/divyagyan/menuexample/MainActivity.java
+++ app/src/main/java/np/edu/divyagyan/menuexample/MainActivity.java
@@ -1,7 +1,17 @@
 package np.edu.divyagyan.menuexample;
 
 import android.os.Bundle;
+import android.view.ContextMenu;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.PopupMenu;
+import android.widget.TextView;
+import android.widget.Button;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -10,5 +20,51 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TextView textView = findViewById(R.id.textView);
+        Button popUpMenuButton = findViewById(R.id.popUpMenuButton);
+
+        popUpMenuButton.setOnClickListener(view -> {
+            PopupMenu popupMenu = new PopupMenu(MainActivity.this, view);
+            popupMenu.inflate(R.menu.popup_menu);
+            popupMenu.show();
+        });
+
+        registerForContextMenu(textView);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.option_menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public void onCreateContextMenu(ContextMenu menu, View v, ContextMenu.ContextMenuInfo menuInfo) {
+        super.onCreateContextMenu(menu, v, menuInfo);
+
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.context_menu, menu);
+    }
+
+    @Override
+    public boolean onContextItemSelected(@NonNull MenuItem item) {
+
+        switch (item.getItemId()) {
+            case R.id.select:
+                Toast.makeText(this, "Select", Toast.LENGTH_SHORT).show();
+                break;
+
+            case R.id.copy:
+                Toast.makeText(this, "Copied", Toast.LENGTH_SHORT).show();
+                break;
+
+            case R.id.cut:
+                Toast.makeText(this, "Cut", Toast.LENGTH_SHORT).show();
+                break;
+        }
+
+        return super.onContextItemSelected(item);
     }
 }
